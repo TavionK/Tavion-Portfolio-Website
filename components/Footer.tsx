374131b5add315bd02f8d8f30a540c97dc12749b
TavionK <tavion.k.britt99@gmail.com>
2026-05-11 16:58:35 -0400

    Add Footer component with copyright notice

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="flex justify-center my-4">
+      Copyright &copy; 2026 Tavion Britt | All Rights Reserved
+    </footer>
+  );
+}
