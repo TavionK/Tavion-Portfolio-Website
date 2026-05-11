99863c3633fd0aa631629bb1010995c8154b5616
TavionK <tavion.k.britt99@gmail.com>
2026-05-11 17:01:13 -0400

    Update text alignment in Home and Footer components

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-dvh">
-      <h1 className='text-6xl font-bold my-4 order-2'>Tavion Britt</h1>
-      <p className='text-2xl order-1'>Coming Soon</p>
+    <div className="flex flex-col items-center justify-center h-dvh text-center">
+      <h1 className="text-6xl font-bold my-4 order-2">Tavion Britt</h1>
+      <p className="text-2xl order-1">Coming Soon</p>
     </div>
   );
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="flex justify-center my-4">
+    <footer className="flex justify-center my-4 text-center">
       Copyright &copy; 2026 Tavion Britt | All Rights Reserved
     </footer>
   );
