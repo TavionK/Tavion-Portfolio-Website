aec15ca950d225696dd1baab2b09f6b23622f2b2
TavionK <tavion.k.britt99@gmail.com>
2026-05-12 16:28:36 -0400

    Update NotFound component layout: improve accessibility and styling

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,11 @@
 export default function NotFound() {
   return (
     <div className="flex flex-col h-dvh items-center justify-center">
-      <h1 className="text-4xl">404 | Page Not Found</h1>
+      <h1 className="sr-only">404 error. Page Not Found</h1>
+      <div className="flex items-center divide-x divide-gray-300">
+        <p className="px-4 text-4xl font-bold">404</p>
+        <p className="px-4 text-xl">Page Not Found</p>
+      </div>
     </div>
   );
 }
